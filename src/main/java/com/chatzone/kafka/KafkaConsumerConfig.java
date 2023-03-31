5a7c0660ec242aab33388c3a366440b234ffec25
duongban <duongban121@gmail.com>
2023-04-01 00:34:39 +0700

    Define kafka consumer config & service

---
+++ src/main/java/com/chatzone/kafka/KafkaConsumerConfig.java
@@ -0,0 +1,72 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2023 duongban.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package com.chatzone.kafka;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+/**
+ *
+ * @author duongban
+ */
+public class KafkaConsumerConfig {
+
+    @Value("${kafka.config.consumer}")
+    private String consumerConfigFilePath;
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        try {
+            Properties props = new Properties();
+            try (InputStream input = getClass().getClassLoader()
+                    .getResourceAsStream(consumerConfigFilePath)) {
+                props.load(input);
+            }
+            return new DefaultKafkaConsumerFactory(props);
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        return null;
+    }
+
+    @Bean
+    public KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+    
+    @Bean
+    public KafkaConsumerService kafkaConsumerService() {
+        return new KafkaConsumerService();
+    }
+}
