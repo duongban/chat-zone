483767772ae8b5529225b224e5f818f451ea6769
duongban <duongban121@gmail.com>
2023-04-04 23:50:28 +0700

    Set websocket allow origin patterns

--- server/src/main/java/com/chatzone/websocket/WebSocketConfig.java
+++ server/src/main/java/com/chatzone/websocket/WebSocketConfig.java
@@ -39,7 +39,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-chat").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
     @Override
