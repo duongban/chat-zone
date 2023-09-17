05e2f5638e7f191c3858a57d1639a8dd769982b7
duongban <duongban121@gmail.com>
2023-09-17 23:30:50 +0700

    (sever): refactor model

--- server/src/main/java/com/chatzone/model/Authen.java
+++ server/src/main/java/com/chatzone/model/Authen.java
@@ -23,28 +23,17 @@
  */
 package com.chatzone.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  *
  * @author duongban
  */
+@Getter
+@Setter
 public class Authen {
 
     private String username;
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- server/src/main/java/com/chatzone/model/Message.java
+++ server/src/main/java/com/chatzone/model/Message.java
@@ -15,39 +15,20 @@
  */
 package com.chatzone.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  *
  * @author duongban
  */
+@Getter
+@Setter
 public class Message {
 
     private String sender;
     private String content;
     private long timestamp;
-
-    public String getSender() {
-        return sender;
-    }
-
-    public void setSender(String sender) {
-        this.sender = sender;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public long getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(long timestamp) {
-        this.timestamp = timestamp;
-    }
 
     @Override
     public String toString() {
