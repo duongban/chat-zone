d74a4e2845f543bfc8b1113d27f5a4bb20cb930e
duongban <duongban121@gmail.com>
2023-10-01 22:27:59 +0700

    (server): add authen filter check session

--- server/src/main/java/com/chatzone/security/SecurityConfig.java
+++ server/src/main/java/com/chatzone/security/SecurityConfig.java
@@ -23,11 +23,13 @@
  */
 package com.chatzone.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -38,10 +40,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    
+    @Autowired
+    private AuthenFilter authenFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable();
+        http.cors().and().csrf().disable()
+                .addFilterBefore(
+                        authenFilter,
+                        UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
