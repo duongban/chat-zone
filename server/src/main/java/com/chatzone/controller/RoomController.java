931b277e4fb6132a23f47a5ae579298e3fa6ac90
duongban <duongban121@gmail.com>
2023-10-03 23:32:48 +0700

    (server): implement api find room by user name

--- server/src/main/java/com/chatzone/controller/RoomController.java
+++ server/src/main/java/com/chatzone/controller/RoomController.java
@@ -28,6 +28,8 @@ import com.chatzone.model.ApiResponse;
 import com.chatzone.model.ECode;
 import com.chatzone.model.Room;
 import com.chatzone.service.inf.IRoomService;
+import java.util.List;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,6 +37,7 @@ import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -56,10 +59,12 @@ public class RoomController {
     private ApiResponse apiResp;
 
     @PostMapping(value = "/room")
-    public ApiResponse create(@RequestBody Room room) {
+    public ApiResponse create(
+            @RequestBody Room room,
+            @RequestAttribute String username) {
         ApiResponse resp = apiResp.getApiResponse(ECode.SUCCESS);
         try {
-            Pair<ECode, RoomEntity> ret = roomService.create(room.getName());
+            Pair<ECode, RoomEntity> ret = roomService.create(room.getName(), username);
             if (ECode.isFailed(ret.getFirst())) {
                 return apiResp.getApiResponse(ret.getFirst());
             }
@@ -85,6 +90,28 @@ public class RoomController {
             Room data = new Room();
             data.setName(ret.getSecond().getName());
             data.setCode(ret.getSecond().getCode());
+            resp.setData(data);
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage(), ex);
+            resp = apiResp.getApiResponse(ECode.EXCEPTION);
+        }
+        return resp;
+    }
+
+    @GetMapping(value = "/rooms")
+    public ApiResponse findRoomByUsername(@RequestAttribute String username) {
+        ApiResponse resp = apiResp.getApiResponse(ECode.SUCCESS);
+        try {
+            Pair<ECode, List<RoomEntity>> ret = roomService.findByCreatedBy(username);
+            if (ECode.isFailed(ret.getFirst())) {
+                return apiResp.getApiResponse(ret.getFirst());
+            }
+            List<Room> data = ret.getSecond().stream().map(item -> {
+                Room room = new Room();
+                room.setName(item.getName());
+                room.setCode(item.getCode());
+                return room;
+            }).collect(Collectors.toList());
             resp.setData(data);
         } catch (Exception ex) {
             LOGGER.error(ex.getMessage(), ex);
