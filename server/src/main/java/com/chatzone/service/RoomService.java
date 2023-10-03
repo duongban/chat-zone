3bfbf8b173597f3f872e5805ac0ba3efc6de7f46
duongban <duongban121@gmail.com>
2023-10-03 23:32:22 +0700

    (server): update create room with user name & implement find room by created by

--- server/src/main/java/com/chatzone/service/RoomService.java
+++ server/src/main/java/com/chatzone/service/RoomService.java
@@ -27,6 +27,8 @@ import com.chatzone.db.entity.RoomEntity;
 import com.chatzone.db.repository.RoomRepository;
 import com.chatzone.model.ECode;
 import com.chatzone.service.inf.IRoomService;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -47,12 +49,14 @@ public class RoomService implements IRoomService {
     private RoomRepository repo;
 
     @Override
-    public Pair<ECode, RoomEntity> create(String name) {
+    public Pair<ECode, RoomEntity> create(String name, String username) {
         Pair<ECode, RoomEntity> ret;
         try {
             RoomEntity data = new RoomEntity();
             data.setName(name);
             data.setCode(UUID.randomUUID().toString());
+            data.setCreatedBy(username);
+            data.setCreatedAt(System.currentTimeMillis());
             ret = Pair.of(ECode.SUCCESS, repo.save(data));
         } catch (Exception ex) {
             LOGGER.error(ex.getMessage(), ex);
@@ -77,4 +81,19 @@ public class RoomService implements IRoomService {
         return ret;
     }
 
+    @Override
+    public Pair<ECode, List<RoomEntity>> findByCreatedBy(String createdBy) {
+        Pair<ECode, List<RoomEntity>> ret;
+        try {
+            List<RoomEntity> data = repo.findByCreatedBy(createdBy);
+            if (data == null) {
+                return Pair.of(ECode.NOT_EXISTS_ROOM, new ArrayList<>());
+            }
+            ret = Pair.of(ECode.SUCCESS, data);
+        } catch (Exception ex) {
+            LOGGER.error(ex.getMessage(), ex);
+            ret = Pair.of(ECode.EXCEPTION, new ArrayList<>());
+        }
+        return ret;
+    }
 }
